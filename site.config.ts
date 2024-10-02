7faeb9125943283d8cfefffbbdd3e35825524e8a
Joshua Carmack <joshuahcarmack@gmail.com>
2024-10-02 09:11:26 -0400

    Updating ID and names

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'd548887b03584b658e44a9fefce30421',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'Joshua Carmack Blog',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  author: 'Joshua Carmack',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
